cdfc6740b8d297d5cabd5660a171c8621fc2dfee
MarcosSilvaDev <marcos.dev1998@gmail.com>
2025-04-12 10:52:23 -0300

    CountDown component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './styles/global.css';
 import './styles/theme.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 function App() {
   return (
@@ -10,7 +12,11 @@ function App() {
         <Logo />
       </Container>
       <Container>
-        <section>Menu</section>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
