12808ca972a53aa0162d7dcf0ccbbffe067adfd7
MarcosSilvaDev <marcos.dev1998@gmail.com>
2025-05-01 18:50:06 -0300

    ContextAPI - useContext + useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useState } from 'react';
-import { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 import './styles/global.css';
 import './styles/theme.css';
-import { TaskContext, TaskContextProvider } from './contexts/TaskContext';
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 function App() {
-  const [state, setState] = useState(initialState);
-
   return (
     <TaskContextProvider>
       <Home />;
