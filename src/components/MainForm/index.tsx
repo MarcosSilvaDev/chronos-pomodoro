9249326e6b69ace9317c2abbb1dedc34cada7e30
MarcosSilvaDev <marcos.dev1998@gmail.com>
2025-05-10 10:13:43 -0300

    Mainform com o input controlado

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,12 +2,28 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefalutButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useRef, useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+  // useRef previne renderizaçoes
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form className='form'>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
-        <DefaultInput labelText='task' id='meuInput' type='text' />
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)} //controla o input
+          ref={taskNameInput} // manda o input para o "taskNameInput"
+        />
       </div>
 
       <div className='formRow'>
