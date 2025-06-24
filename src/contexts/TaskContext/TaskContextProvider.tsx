dff953b2486063336d400bbe51bbb790cff688ea
MarcosSilvaDev <marcos.dev1998@gmail.com>
2025-06-23 21:04:07 -0300

    Atualizando o contador a cada segundo na pagina

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -3,6 +3,7 @@ import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
+import { TaskActionTypes } from './taskAction';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -17,11 +18,20 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     console.log(countDownSeconds);
 
     if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
       worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
     }
   });
 
   useEffect(() => {
+    console.log(state);
     if (!state.activeTask) {
       console.log('Worker finalizado, falta de ativeTask');
       worker.terminate();
